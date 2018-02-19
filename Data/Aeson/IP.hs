596259cc51809f82b99ba9fbc4b9ea535b18c42f
Lana Black <lanablack@amok.cc>
2018-02-19 13:07:52 +0000

    Add ToJSONKey and FromJSONKey instances.

--- Data/Aeson/IP.hs
+++ Data/Aeson/IP.hs
@@ -19,8 +19,17 @@ instance FromJSON IPv4 where
         | otherwise = fail "Unable to parse"
     parseJSON v = typeMismatch "IPv4" v
 
+instance FromJSONKey IPv4 where
+    fromJSONKey = FromJSONKeyTextParser $ \t ->
+                      case readMaybe (Text.unpack t) of
+                          Just r -> pure r
+                          Nothing -> fail "Unable to parse IPv4"
+
 instance ToJSON IPv4 where
     toJSON = String . Text.pack . show
+
+instance ToJSONKey IPv4 where
+    toJSONKey = toJSONKeyText (Text.pack . show)
 
 instance FromJSON IPv6 where
     parseJSON (String s)
@@ -28,8 +37,17 @@ instance FromJSON IPv6 where
         | otherwise = fail "Unable to parse"
     parseJSON v = typeMismatch "IPv6" v
 
+instance FromJSONKey IPv6 where
+    fromJSONKey = FromJSONKeyTextParser $ \t ->
+                      case readMaybe (Text.unpack t) of
+                          Just r -> pure r
+                          Nothing -> fail "Unable to parse IPv6"
+
 instance ToJSON IPv6 where
     toJSON = String . Text.pack . show
+
+instance ToJSONKey IPv6 where
+    toJSONKey = toJSONKeyText (Text.pack . show)
 
 instance FromJSON IP where
     parseJSON (String s)
@@ -37,8 +55,17 @@ instance FromJSON IP where
         | otherwise = fail "Unable to parse"
     parseJSON v = typeMismatch "IP" v
 
+instance FromJSONKey IP where
+    fromJSONKey = FromJSONKeyTextParser $ \t ->
+                      case readMaybe (Text.unpack t) of
+                          Just r -> pure r
+                          Nothing -> fail "Unable to parse IP"
+
 instance ToJSON IP where
     toJSON = String . Text.pack . show
+
+instance ToJSONKey IP where
+    toJSONKey = toJSONKeyText (Text.pack . show)
 
 instance Read (AddrRange a) => FromJSON (AddrRange a) where
     parseJSON (String s)
@@ -46,7 +73,7 @@ instance Read (AddrRange a) => FromJSON (AddrRange a) where
         | otherwise = fail "Unable to parse"
     parseJSON v = typeMismatch "AddrRange" v
 
-instance Show (AddrRange a) => ToJSON (AddrRange a) where
+instance Show a => ToJSON (AddrRange a) where
     toJSON = String . Text.pack . show
 
 instance FromJSON IPRange where
@@ -58,3 +85,20 @@ instance FromJSON IPRange where
 instance ToJSON IPRange where
     toJSON = String . Text.pack . show
 
+instance Read (AddrRange a) => FromJSONKey (AddrRange a) where
+    fromJSONKey = FromJSONKeyTextParser $ \t ->
+                      case readMaybe (Text.unpack t) of
+                          Just r -> pure r
+                          Nothing -> fail "Unable to parse AddrRange"
+
+instance Show a => ToJSONKey (AddrRange a) where
+    toJSONKey = toJSONKeyText (Text.pack . show)
+
+instance FromJSONKey IPRange where
+    fromJSONKey = FromJSONKeyTextParser $ \t ->
+                      case readMaybe (Text.unpack t) of
+                          Just r -> pure r
+                          Nothing -> fail "Unable to parse IPRange"
+
+instance ToJSONKey IPRange where
+    toJSONKey = toJSONKeyText (Text.pack . show)
